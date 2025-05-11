6596d06966f15c48b62bd4f545364bb72107e048
Pratik Kumbhar <115476295+pkpotter03@users.noreply.github.com>
2025-05-11 11:22:56 +0000

    add logger file with morgan and winston

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,5 +1,7 @@
 import 'dotenv/config';
 import express from "express";
+import logger from './logger.js';
+import morgan from 'morgan';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +11,22 @@ let nextId = 0;
 
 app.use(express.json());
 
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+}));
 app.listen(port, () => {
   console.log(`server started at ${port}`);
 });
@@ -33,23 +51,23 @@ app.post("/teas", (req, res) => {
 });
 
 app.put("/teas/:id", (req, res) => {
-   const tea = teadata.find(t => t.id === parseInt(req.params.id))
+  const tea = teadata.find(t => t.id === parseInt(req.params.id))
   if (!tea) {
     res.status(404).send("tea not found");
   }
 
-  const {name, price} = req.body
+  const { name, price } = req.body
   tea.name = name
   tea.price = price
 
   res.status(200).send(tea);
 });
 
-app.delete('/teas/:id',(req, res) => {
-    const index = teadata.findIndex(t => t.id === parseInt(req.params.id))
-    if(index === -1){
-        return res.status(404).send("tea not found")
-    }
-    teadata.splice(index, 1)
-    return res.status(204).send("deleted")
+app.delete('/teas/:id', (req, res) => {
+  const index = teadata.findIndex(t => t.id === parseInt(req.params.id))
+  if (index === -1) {
+    return res.status(404).send("tea not found")
+  }
+  teadata.splice(index, 1)
+  return res.status(204).send("deleted")
 })
