9362e455de177106e23868dd13d55048ed7faf57
Pratik Kumbhar <115476295+pkpotter03@users.noreply.github.com>
2025-05-31 15:14:39 +0000

    connect MongoDB Database

--- VIDTUBE/src/index.js
+++ VIDTUBE/src/index.js
@@ -1,1 +1,19 @@
-console.log("ke haal chaal ?!!");
+import dotenv from "dotenv"
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "src/.env"
+})
+const PORT = process.env.PORT || 8001;
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB Connection Error", err);
+    })
+
