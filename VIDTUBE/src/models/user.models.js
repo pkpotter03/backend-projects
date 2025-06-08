a1b7a751bcdfe1d65ddcb4e97c7bbc9e91d1231e
Pratik Kumbhar <115476295+pkpotter03@users.noreply.github.com>
2025-06-08 10:06:53 +0000

    add methods using jwt and bcrypt in userModels

--- VIDTUBE/src/models/user.models.js
+++ VIDTUBE/src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose"
-
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const userSchema = new Schema(
     {
         username: {
@@ -46,5 +47,36 @@ const userSchema = new Schema(
     },
     { timestamps: true }
 )
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.modified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.method.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
+}
+
+userSchema.method.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY });
+}
 
 export const User = mongoose.model("User", userSchema)
