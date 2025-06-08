afe64c5fbbc6623c4d5ba4a5ec2429745d83acbc
Pratik Kumbhar <115476295+pkpotter03@users.noreply.github.com>
2025-06-08 18:23:32 +0000

    remove VIDTUBE and create new seperate repository for it

--- VIDTUBE/src/routes/healthcheck.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { healthcheck } from "../controllers/healthcheck.controllers.js"
-
-const healthcheckRouter = Router()
-
-healthcheckRouter.route("/").get(healthcheck)
-
-export default healthcheckRouter
